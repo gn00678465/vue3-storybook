c23d920eebae491084159b38377d82af31391546
FuYu <gn02515634@gmail.com>
2022-04-01 08:16:16 +0800

    add scss support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,10 +7,18 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
   ],
   "framework": "@storybook/vue3",
   "core": {
     "builder": "webpack5"
+  },
+  webpackFinal: async(config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
   }
 }
