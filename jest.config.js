d548c03e5117a937457654e37ff7c521d05b4013
Madao <gn00678465@gmail.com>
2022-04-03 17:49:31 +0800

    feat: Adjustment Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,30 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
-  collectCoverage: true
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)"
+  ],
+  collectCoverage: true,
+  "collectCoverageFrom": [
+    "**/*.{js,vue}",
+    "!**/node_modules/**",
+    '!**/build/**',
+    // '!**/src/**',
+    '!**/build/**',
+    '!**/stories/**',
+    '!**/coverage/**',
+    '!**/.storybook/**',
+    '!.eslintrc.js',
+    "!babel.config.js",
+    "!jest.config.js",
+    "!postcss.config.js",
+  ],
+  "moduleFileExtensions": [
+    "js",
+    "json",
+    "vue"
+  ],
+  "transform": {
+    ".*\\.(vue)$": "@vue/vue3-jest"
+  }
 };
